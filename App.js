d3228c763bd078e5b73adc9ed3b06c5a50cf146f
PoojithaUnnam08 <poojithaunnam.darshanelderscare@gmail.com>
2024-08-08 14:20:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,55 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext'; // Import CartProvider
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Homecare from './pages/Homecare';
+import Bloodtest from './pages/Bloodtest';
+import Reports from './pages/Reports';
+import Physio from './pages/Physio';
+import Elderscare from './pages/Elderscare';
+import Oldagehome from './pages/Oldagehome';
+import Medicalequipments from './pages/Medicalequipments';
+import Nursesathome from './pages/Nursesathome';
+import Doctorsathome from './pages/Doctorsathome';
+import Labservices from './pages/Labservices';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <Router>
+            <CartProvider> {/* Wrap with CartProvider */}
+                <div className="App">
+                    <Navbar />
+                    <main className="pt-16">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/homecare" element={<Homecare />} />
+                            <Route path="/services" element={<Services />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/bloodtest" element={<Bloodtest />} />
+                            <Route path="/reports" element={<Reports />} />
+                            <Route path="/physio" element={<Physio />} />
+                            <Route path="/elderscare" element={<Elderscare />} />
+                            <Route path="/oldagehome" element={<Oldagehome />} />
+                            <Route path="/medicalequipments" element={<Medicalequipments />} />
+                            <Route path="/cart" element={<Cart />} />
+                            <Route path="/checkout" element={<Checkout />} />
+                        </Routes>
+                    </main>
+                </div>
+            </CartProvider>
+        </Router>
+    );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-import electricWheelChair from './electric wheel chair.jpg';
-import hospitalCot from './hospital cot.jpg';
-import wheelChair from './wheel chair.jpg';
-import walker from './walker.jpg';
-import glucometer from './glucometer.jpg';
-import glucometerStrips from './glucometer strips.jpg';
-import bpMachine from './bp machine.jpg';
-import himalayagentlewipes from './himalayagentlewipes.jpg'; // Ensure the correct file extension
-import babycare from './babycare.jpg';
-import metalwalker from './metalwalker.jpg';
-import stainlesssteelwalker from './stainlesssteelwalker.jpg';
-import drorthobacksupportbelt from './drorthobacksupportbelt.jpg';
-import anklesupport from './anklesupport.jpg';
-import armwrist from './armwrist.jpg';
-import wristbrace from './wristbrace.jpg';
-import wristsplint from './wristsplint.jpg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export {
-    electricWheelChair,
-    hospitalCot,
-    wheelChair,
-    walker,
-    glucometer,
-    glucometerStrips,
-    bpMachine,
-    himalayagentlewipes,
-    babycare,
-    metalwalker,
-    stainlesssteelwalker,
-    drorthobacksupportbelt,
-    anklesupport,
-    armwrist,
-    wristbrace,
-    wristsplint
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
